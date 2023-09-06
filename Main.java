e6b7dfd60f8ed214ff0c944a634e395e581da1d1
Kristopher Williams <kristopher.williams@doane.edu>
2023-09-06 14:34:41 -0500

    Trial commit

--- Main.java
+++ Main.java
@@ -3,5 +3,6 @@ public class Main {
     //some sort of SLL of books to keep them in order
     //prompt user to enter # and size of shelves
     //randomly split books to shelves
+    // kw trial 
     
 }
