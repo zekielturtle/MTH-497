18b3609a95ab7c813d9bbafc36ae22552ed9c065
zekielturtle <zekiel.turtle@gmail.com>
2023-09-12 15:33:30 -0500

    added things

--- Book.java
+++ Book.java
@@ -1,38 +1,82 @@
-public class Book {
+
+
+public class Book implements Comparable<Book> {
     private String title;
     private String callNumber;
     private String author;
     private int pages;
-    private int size;
-    private Shelf shelf;
+    private int size; //in cm
+    private int id;
+    //maybe a bool isSet or something with other books in the set?
 
-    public Book(String title, String author, String callNumber, int pages){
+    public Book(String title, String author, String callNumber, int pages, int id){
         this.title = title;
         this.author = author;
         this.callNumber = callNumber;
         this.pages = pages;
+        this.id = id;
         calcSize();
+    }
+
+    public Book(String title, String author, String callNumber, int id){
+        this.title = title;
+        this.author = author;
+        this.callNumber = callNumber;
+        this.id = id;
+        size = 20; //placeholder value
     }
 
     public String getTitle(){
         return title;
     }
 
+    public void setTitle(String title){
+        this.title = title;
+    }
+
     public String getCallNumber(){
         return callNumber;
+    }
+
+    public void setCallNumber(String callNumber){
+        this.callNumber = callNumber;
     }
 
     public String getAuthor(){
         return author;
     }
 
+    public void setAuthor(String author){
+        this.author = author;
+    }
+
     public int getSize(){
         return size;
     }
 
-    private void calcSize() {
-        int s;
-        //code to calculate the size
-        size = s;
+    public void setSize(int size){
+        this.size = size;
+    }
+
+    private int calcSize() {
+        int p = pages;
+        //code to calculate the size - likely just a linear equation from a regression model determined elsewhere
+        return 20; //placeholder value for now
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+    @Override public int compareTo(Book o) {
+        return this.id - o.id; //placeholder sorting
+    }
+
+    @Override public String toString(){
+        return title + " by " + author;
     }
 }
--- Main.java
+++ Main.java
@@ -1,7 +1,16 @@
+import java.util.*;
+import java.io.*;
+
 public class Main {
+    public static void main(String[] args){
     //read in database
+    Scanner scan = new Scanner("sample.tsv");
+    scan.useDelimiter("\t");
+    while (scan.hasNext()){
+        System.out.println(scan.next());
+    }
     //some sort of SLL of books to keep them in order
     //prompt user to enter # and size of shelves
     //randomly split books to shelves
-    
+    }
 }
